cc48fe83d31fc664d52924734bf64343a53278dc
Sayantan Das <sayantan.mixed@gmail.com>
2026-08-06 23:59:45 +0530

    Time: 0 ms (100%), Space: 43.4 MB (25.37%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int c=0,ans=0;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
+                c++;
+                ans=ans+c;
+            }else{
+                c=0;
+            }
+        }
+    
+    return ans;
+    }
+}
